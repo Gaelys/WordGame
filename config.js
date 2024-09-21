4e70e50e2958a025fe28a720cf9f81ebfb4c7286
Charlotte GAERTNER <charlotte.gaertner@outlook.fr>
2024-09-21 16:32:25 +0200

    first try, add a file to contain the lists

---
+++ config.js
@@ -0,0 +1,4 @@
+//All of our lists are stored here, so that we can easily change them later on if needed
+
+const wordsList = ['Cachalot', 'Pétunia', 'Serviette'];
+const phrasesList = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"];
